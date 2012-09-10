6257f25ea52b50d6a479448d675bc6bb450d3044
John Pucciarelli <john.pucciarelli@gmail.com>
2012-09-09 20:30:09 -0400

    added ejs template support. using npm packages for backbone / require

--- server/app.js
+++ server/app.js
@@ -1,25 +1,16 @@
 "use strict";
 
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var port = 3000;
 
-
-/* TODO: get underscore templates rendered
-var _ = require('../lib/underscore/underscore');
-app.engine('.html', {
-  compile: function (str, options) {
-    var template = _.template(str);
-    return function (locals) {
-      return template(locals);
-    };
-  }
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	app.render('home/index');
+	res.render('index');
 });
-*/
 
 app.listen(port);
 console.log('Listening on port ' + port);
