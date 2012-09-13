81a54e468b861fab622ac9d54ce2e209e566b0ac
John Pucciarelli <john.pucciarelli@gmail.com>
2012-09-13 19:18:58 -0400

    swapped out ejs for dust, dust running through consolidate wrapper

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ requirejs.config({
 	nodeRequire: require,
 });
 
-requirejs(['express', 'underscore', 'backbone'], function(express, _, Backbone) {
+requirejs(['express', 'consolidate', 'underscore', 'backbone'], function(express, consolidate, _, Backbone) {
 
 	var App = Backbone.Model.extend({
 
@@ -16,11 +16,18 @@ requirejs(['express', 'underscore', 'backbone'], function(express, _, Backbone) 
 		},
 
 		initialize: function() {
+			var that = this;
 			this.app = express();
-			this.app.set('views', __dirname + '/views');
-			this.app.set('view engine', 'ejs');
+			this.app.engine('dust', consolidate.dust);
+			this.app.configure(function() {
+			    that.app.set('view engine', 'dust');
+			    that.app.set('views', __dirname + '/views');
+			    that.app.use(express.static(__dirname + '/public', { redirect: false })); /*TODO: <-- wtf? */
+			    that.app.use(express.bodyParser()); /*TODO: <-- wtf? */
+			 });
+			var numClients = 0;
 			this.app.get('/', function(req, res) {
-				res.render('index');
+				res.render('index', { numClients: ++numClients });
 			});
 			this.app.listen(this.get('port'));
 			console.log('Listening on port ' + this.get('port'));
--- start.js
+++ start.js
@@ -38,4 +38,4 @@ console.log('\n\
  \\__,_|\\__,_|\\__, /___\\___|   \\_/  \\___(_)___(_)_| \n\
              |___/                                 \n\
 ');
-console.log('\nInstalling dependencies and launching server...')
+console.log('\nInstalling dependencies, bundling client libraries and launching server...')
