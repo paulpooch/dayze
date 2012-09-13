4b5741c0c7f87211699d806103150624e261424d
John Pucciarelli <john.pucciarelli@gmail.com>
2012-09-13 19:30:23 -0400

    cleaned app.js, added comments

--- server/app.js
+++ server/app.js
@@ -4,10 +4,10 @@ var requirejs = require('requirejs');
 
 requirejs.config({
 	baseUrl: __dirname,
-	nodeRequire: require,
+	nodeRequire: require,	// tell requirejs to use node's 'require()'
 });
 
-requirejs(['express', 'consolidate', 'underscore', 'backbone'], function(express, consolidate, _, Backbone) {
+requirejs(['express', 'consolidate', 'underscore', 'backbone'], function(express, consolidate, _, Backbone) {	// list all dependencies for this scope
 
 	var App = Backbone.Model.extend({
 
@@ -16,19 +16,25 @@ requirejs(['express', 'consolidate', 'underscore', 'backbone'], function(express
 		},
 
 		initialize: function() {
-			var that = this;
+
+			var that = this;																// used inside anonymous functions
+
+			// configure express
 			this.app = express();
 			this.app.engine('dust', consolidate.dust);
 			this.app.configure(function() {
 			    that.app.set('view engine', 'dust');
 			    that.app.set('views', __dirname + '/views');
-			    that.app.use(express.static(__dirname + '/public', { redirect: false })); /*TODO: <-- wtf? */
-			    that.app.use(express.bodyParser()); /*TODO: <-- wtf? */
+			    that.app.use(express.bodyParser()); 										// now have access to dom via req.body.title, etc...
 			 });
+
+			// handle requests to root
 			var numClients = 0;
 			this.app.get('/', function(req, res) {
 				res.render('index', { numClients: ++numClients });
 			});
+
+			// begin listening
 			this.app.listen(this.get('port'));
 			console.log('Listening on port ' + this.get('port'));
 		}
